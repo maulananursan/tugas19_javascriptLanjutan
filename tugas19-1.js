e265d5b10e605c797df7cef0e0e1b8eac1735019
Maulana Nursan <71365575+maulananursan@users.noreply.github.com>
2020-09-21 11:49:50 +0700

    Add files via upload

---
+++ tugas19-1.js
@@ -0,0 +1,4 @@
+var kata = "Semangat..."
+
+ulang = kata.repeat(5)
+console.log(ulang)
---
+++ tugas19-2.js
@@ -0,0 +1,9 @@
+var bintang = "*"
+
+console.log(bintang.repeat(5))
+console.log(bintang.repeat(15))
+console.log(bintang.repeat(25))
+console.log(bintang.repeat(35))
+console.log(bintang.repeat(25))
+console.log(bintang.repeat(15))
+console.log(bintang.repeat(5))
